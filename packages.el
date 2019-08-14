18c6657b3744ca2606a3a1881ab750151207ffb0
a5l3 <43312868+a5l3@users.noreply.github.com>
2019-08-14 23:03:28 +0300

    add packages.el

---
+++ packages.el
@@ -0,0 +1,31 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+
+
+(package! ob-typescript)
+(package! js2-mode)
+(package! js-import)
+(package! web-mode)
+(package! react-snippets)
+(package! company-tern)
+(package! rjsx-mode)
+(package! indium)
+(package! reason-mode)
+(package! prettier-js)
+(package! org-fancy-priorities)
+(package! move-text)
+(package! origami)
+(package! org-super-agenda)
+(package! dart-mode)
+(package! ob-dart)
+(package! org-brain)
+(package! ascii-art-to-unicode)
+(package! wakatime-mode)
+(package! doom-themes)
+(package! eslint-fix)
+
